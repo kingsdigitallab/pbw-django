427b86a5e980d99b81b61935eb04443b54d24bb8
elliotthall <elliotthall@KCLC02KV12EF6T5.local>
2016-07-06 12:14:47 +0100

    Initial commit from bare bones

---
+++ pbw-django/settings/stg.py
@@ -0,0 +1,27 @@
+from base import *  # noqa
+
+CACHE_REDIS_DATABASE = '1'
+CACHES['default']['LOCATION'] = '127.0.0.1:6379:' + CACHE_REDIS_DATABASE
+
+INTERNAL_IPS = INTERNAL_IPS + ('', )
+ALLOWED_HOSTS = ['']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'app_pbw-django_stg',
+        'USER': 'app_pbw-django',
+        'PASSWORD': '',
+        'HOST': ''
+    },
+}
+
+
+# -----------------------------------------------------------------------------
+# Local settings
+# -----------------------------------------------------------------------------
+
+try:
+    from local import *  # noqa
+except ImportError:
+    pass
