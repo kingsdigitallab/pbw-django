a08891e209134de956842992ff549290e0495814
ehall <elliott.hall@kcl.ac.uk>
2017-05-09 10:35:59 +0000

    Fixed integrity error with new scdate

--- pbw/management/commands/add_sc_dates.py
+++ pbw/management/commands/add_sc_dates.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.db.utils import IntegrityError
+import pdb
 
-from pbw.models import Scdate, Scsource, Datetypes, Narrativefactoid
+from pbw.models import Scdate, Scsource, Factoid, Datetypes, Narrativefactoid
 
 
 class Command(BaseCommand):
@@ -9,24 +10,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Get all factoids without scdates
-        paginate = 100
-        limit = paginate
-        counter = 0
-        offset = 0
-        total = 10000  # Factoid.objects.count()
-        nssource = Scsource.objects.get_or_create(ssourceid='Narrative Units',ssourcefullref='')
+        paginate = 1000
+        offset = 10000
+        counter = offset
+        limit = offset + paginate
+        total = Factoid.objects.count()
+        nssource = Scsource.objects.get_or_create(ssourceid='Narrative Units', ssourcefullref='')[0]
         while offset < total:
             for nf in Narrativefactoid.objects.filter(narrativeunit__yearorder__gt=0)[offset:limit]:
                 try:
                     if not Scdate.objects.filter(factoid=nf.factoid).exists():
-                        # Find an attached narrative unit if it exists
-                        # (split these up because both at once caused performance problems)
-                        # years = Narrativeunit.objects.filter(narrativefactoid__factoid=nf.factoid, yearorder__gt=0)
-                        # if years.count() > 1:
-                        #     print ("Multiple units for factoid {}\n".format(nf.factoid.pk))
-                        #     for year in years:
-                        #         print ("{}: {}\n\n".format(year.pk, year.yearorder))
-                        # elif years.count() > 0:
                         year = nf.narrativeunit
                         # Set the scdate to that year
                         if year.datetypekey:
@@ -37,20 +30,18 @@ class Command(BaseCommand):
                                           notes='Created from narrative dates',
                                           ssourcekey=nssource.ssourcekey,
                                           year=year.yearorder,
+                                          yrorder=1,
+                                          priority=1,
+                                          acckey=1,
                                           datetype=date_type)
                         new_date.save()
                         print("New date for P{}".format(nf.factoid.pk))
                     counter += 1
                 except IntegrityError as IE:
-                    import pdb; pdb.set_trace()
-                    print "Integrity Error factoid: {}".format(nf.factoid.pk)
+                    pdb.set_trace()
+                    print ("Integrity Error factoid: {}".format(nf.factoid.pk))
 
             offset = limit
             limit += paginate
 
-            print "Processed {}\n".format(counter)
-
-
-
-            # Set certainty
-            # save
+            print ("Processed {}\n".format(counter))
