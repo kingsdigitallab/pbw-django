09af1c93d16bef74614ce86773f724d2cf372ab9
Elliott Hall <elliott.hall@kcl.ac.uk>
2025-03-26 14:01:35 +0000

    scraped htmls simplified

--- pbw2011/_a/s/sifr-setup.js
+++
@@ -1,3 +0,0 @@
-if(typeof sIFR == "function"){
-sIFR.replaceElement(named({sSelector:"#mainFrame h1 span", sFlashSrc:"_a/s/sifr-baskerville.swf", sColor:"#01005d", sLinkColor:"#01005d", sBgColor:"#ffffff", sHoverColor:"#666699", sWmode:"transparent", nPaddingTop:0, nPaddingBottom:0, sFlashVars:"textalign=left&offsetTop=1&offsetLeft=0&offsetRight=0"}));
-};
